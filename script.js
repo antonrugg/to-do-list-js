0a90857eff6b24ac0692b72125a79c7388579b4f
Antonio Ruggiero <antonrugg@gmail.com>
2022-03-05 18:10:23 +0100

    listing - new to do - delete

--- script.js
+++ script.js
@@ -1,1 +1,28 @@
-console.log('JS OK');
+let input = prompt('What would you like to do?');
+const todos = ['prova', 'prova prova'];
+while (input !== 'quit' && input !== 'q') {
+    if (input === 'list') {
+        console.log('********************')
+        for (let i = 0; i < todos.length; i++)
+        {
+            console.log(`${i}: ${todos[i]}`)
+            }
+        console.log('*******************')
+    } else if(input === 'new'){
+        const newTodo = prompt('Ok, what is the new to do?');
+        todos.push(newTodo);
+        console.log(`${newTodo} added to the list`);
+    } else if (input === 'delete') {
+        const index = parseInt(prompt('Ok, enter the index to delete'));
+        if (!Number.isNaN(index)) {   
+            const deleted = todos.splice(index, 1);
+            console.log(`Ok, deleted ${deleted[0]}`);
+        } else {
+            console.log('Unknown index');
+        }
+    }
+
+
+    input = prompt('What would you like to do?')
+}
+console.log('Ok, quit the app.');
